7092c0eff3b62979cfc7397fb8e3832cf0cd9c16
mib <mib@mib.dev>
2021-01-21 23:19:03 +0100

    Update db adapter to cope with new schema

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ extern crate diesel;
 #[macro_use]
 extern crate rocket_contrib;
 
+use rand::{prelude::*, Rng, distributions::Alphanumeric};
 use diesel::prelude::*;
 
 pub mod schema;
 pub mod models;
 
 use models::*;
+
+const ID_LENGTH: usize = 5;
  
 #[database("redirect_db")]
 pub struct RedirectDb(diesel::pg::PgConnection);
@@ -22,10 +25,20 @@ impl RedirectDb {
             .load::<Redirect>(conn)
     }
 
-    pub fn create_redirect(&self, url: &str) -> QueryResult<i32> {
+    pub fn create_redirect(&self, url: &str) -> QueryResult<String> {
         let conn = &self.0;
 
+        // inefficient. has to wait for system entropy as well
+        // consider using nanoid crate
+        let rng = SmallRng::from_entropy(); 
+        let random_id: String = rng
+            .sample_iter(&Alphanumeric)
+            .take(ID_LENGTH)
+            .map(char::from)
+            .collect();
+
         let new_redirect = NewRedirect {
+            id: random_id,
             url: url.to_string(),
         };
 
@@ -35,7 +48,7 @@ impl RedirectDb {
             .get_result(conn)
     }
 
-    pub fn get_redirect_with_id(&self, id: i32) -> QueryResult<Redirect> {
+    pub fn get_redirect_with_id(&self, id: &str) -> QueryResult<Redirect> {
         let conn = &self.0;
 
         redirects::table
